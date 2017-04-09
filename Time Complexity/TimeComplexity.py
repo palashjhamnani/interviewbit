72044045247ee1a82442e53005ab563d0c476ce8
Palash Jhamnani <palashjhamnani@gmail.com>
2017-04-09 13:53:11 +0530

    Added comments

--- Time Complexity/TimeComplexity.py
+++ Time Complexity/TimeComplexity.py
@@ -1,10 +1,18 @@
 import time
 import math
 
+
+# Two methods to check weather a number is prime or not
+# First: Check division with every number less than itself
+# Second: Check division with every number less than its square root
+
 t1 = time.time()
 i = 2
 #N = 1000033
+# Make N very large, the First approach won't give an answer, it will take a lot of time due to large input size and time complexity
+
 N = 1500450271
+#while i < N:
 while i < math.sqrt(N):
     if N % i == 0:
         print "Not prime"
@@ -17,3 +25,11 @@ while i < math.sqrt(N):
 t2 = time.time()
 t = t2-t1
 print "Time taken = "+ str(t)+"Seconds"
+
+
+#To test time difference
+#t1 = time.time()
+#time.sleep(2)
+#t2 = time.time()
+#t = t2-t1
+#print t
